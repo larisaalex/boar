cb3048058f2465843a2d5e3e6e197c1d201c58f4
Larisa test <alexandrova.uu@yandex.ru>
2023-06-20 16:57:25 +0300

    change boar

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         int start = 90;
-
-        int replenishment = 1500;
-
-        int bonus;
-        if (replenishment > 1000){
-            bonus = 100 * 1;
+        int replenishment = 1200;
+        int bonuscoefficient = 100;
+        int threshold = 1000;
+        if (replenishment > threshold) {
+            int general = replenishment / bonuscoefficient + replenishment + start;
+            System.out.println("Итоговый счёт: " + general + "рублей");
         } else {
-            bonus = 1;
+            int general = replenishment + start;;
+            System.out.println("Итоговый счёт: " + general + "рублей");
         }
-        int check = replenishment + start;
-
-        int general = check + bonus;
-
-        System.out.println("Итоговый счёт: " + general);
     }
 }
